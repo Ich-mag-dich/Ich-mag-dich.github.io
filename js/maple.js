69d7171f0fd22308cc0896d7867b01769e92204d
asdasd <92120487+Ich-mag-dich@users.noreply.github.com>
2021-11-02 15:42:07 +0900

    import character card

--- js/maple.js
+++ js/maple.js
@@ -1,67 +1,71 @@
 function getCharImg() {
-    let charNum = 1;
-    var charName = document.getElementById("fname");
+  let charNum = 1;
+  var charName = document.getElementById("fname");
 
-    if (charName.value == '' || charName.value == null) {
-        alert( '이름을 입력해주세요' );
-        return false;
-    }
-    var xmlHttp = new XMLHttpRequest();
+  if (charName.value == "" || charName.value == null) {
+    alert("이름을 입력해주세요");
+    return false;
+  }
+  var xmlHttp = new XMLHttpRequest();
 
-    xmlHttp.onreadystatechange = function () {
+  xmlHttp.onreadystatechange = function () {
     if (xmlHttp.readyState === xmlHttp.DONE) {
-        if (xmlHttp.status === 200) {
-            console.log(xmlHttp.status);
-            console.log(xmlHttp.responseText);
-            var el = document.createElement( 'html' );
-            el.innerHTML = xmlHttp.responseText;
-            if (el.querySelector("#container > div > div > div:nth-child(4) > div").innerText == '랭킹정보가 없습니다.') {
-                document.getElementById("charImg").src = './img/error.png';
-            document.getElementById("charImg").style.display = 'unset';
-            } else {
-                for (i=1; i<11; i++) {
-                    var getcharName = el.querySelector(`#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr:nth-child(${i}) > td.left > dl > dt > a`).innerText
-                    if (getcharName == charName.value) {
-                        charNum = i;
-                        console.log(i);
-                        console.log(getcharName);
-                    } else {
-                        if (i == 10) {
-                            document.getElementById("charImg").src = './img/error.png';
-                            document.getElementById("charImg").style.display = 'unset';
-                            return 0;
-                        }
-                    }
-                }
-                getImg = el.querySelector(`#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr:nth-child(${charNum}) > td.left > span > img:nth-child(1)`).src;      
-                //console.log(el.querySelector("#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr:nth-child(10) > td.left > dl > dt > a"));
-                document.getElementById("charImg").src = getImg;
-                document.getElementById("charImg").style.display = 'unset';
-                //document.getElementById("characterName").innerText = getcharName;
-                //document.getElementById("characterName").style.display = 'unset';
-                //document.getElementById("charBackground").style.display = 'unset';
-                
-            }
+      if (xmlHttp.status === 200) {
+        console.log(xmlHttp.status);
+        console.log(xmlHttp.responseText);
+        var el = document.createElement("html");
+        el.innerHTML = xmlHttp.responseText;
+        if (
+          el.querySelector("#container > div > div > div:nth-child(4) > div")
+            .innerText == "랭킹정보가 없습니다."
+        ) {
+          document.getElementById("charImg").src = "./img/error.png";
+          document.getElementById("charImg").style.display = "unset";
         } else {
-            document.getElementById("charImg").src = './img/error.png';
-            document.getElementById("charImg").style.display = 'unset';
-            console.log(xmlHttp.status);
+          for (i = 1; i < 11; i++) {
+            var getcharName = el.querySelector(
+              `#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr:nth-child(${i}) > td.left > dl > dt > a`
+            ).innerText;
+            if (getcharName == charName.value) {
+              charNum = i;
+              console.log(i);
+              console.log(getcharName);
+            } else {
+              if (i == 10) {
+                document.getElementById("charImg").src = "./img/error.png";
+                document.getElementById("charImg").style.display = "unset";
+                return 0;
+              }
+            }
+          }
+          getImg = el.querySelector(
+            `#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr:nth-child(${charNum}) > td.left > span > img:nth-child(1)`
+          ).src;
+          //console.log(el.querySelector("#container > div > div > div:nth-child(4) > div.rank_table_wrap > table > tbody > tr:nth-child(10) > td.left > dl > dt > a"));
+          document.getElementById("charImg").src = getImg;
+          document.getElementById("charImg").style.display = "unset";
+          //document.getElementById("characterName").innerText = getcharName;
+          //document.getElementById("characterName").style.display = 'unset';
+          //document.getElementById("charBackground").style.display = 'unset';
         }
+      } else {
+        document.getElementById("charImg").src = "./img/error.png";
+        document.getElementById("charImg").style.display = "unset";
+        console.log(xmlHttp.status);
+      }
     }
-    };
-    xmlHttp.open( "GET", `https://cors-anywhere.herokuapp.com/https://maplestory.nexon.com/Ranking/World/Total?c=${charName.value}&w=0`);
-    xmlHttp.setRequestHeader("origin", "://");
-    xmlHttp.send(null);
-
-
-
+  };
+  xmlHttp.open(
+    "GET",
+    `https://cors-anywhere.herokuapp.com/https://maplestory.nexon.com/Ranking/World/Total?c=${charName.value}&w=0`
+  );
+  xmlHttp.setRequestHeader("origin", "://");
+  xmlHttp.send(null);
 }
-$( function() {
-    $( "#charBackground" ).draggable();
-  } );
+$(function () {
+  $("#charBackground").draggable();
+});
 
-$( function() {
-    $( "#charImg" ).draggable();
-  } );
-
-
+$(function () {
+  $("#charImg").draggable();
+});
---
+++ js/mapletest.js
@@ -0,0 +1,90 @@
+function ggcharImg() {
+  var charName = document.getElementById("fname");
+
+  if (charName.value == "" || charName.value == null) {
+    alert("이름을 입력해주세요");
+    return false;
+  }
+  var xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState === xmlHttp.DONE) {
+      if (xmlHttp.status === 200) {
+        console.log(xmlHttp.status);
+        //console.log(xmlHttp.responseText);
+        var el = document.createElement("html");
+        el.innerHTML = xmlHttp.responseText;
+        var getgg = el.querySelector("#character-card");
+        $("#imgDiv").append(getgg);
+        if (
+          document.querySelector(
+            "#character-card > div > ul.character-card-summary > li:nth-child(1) > span"
+          ) == 베라
+        ) {
+          server =
+            "https://ssl.nexon.com/s2/game/maplestory/renewal/common/world_icon/icon_12.png";
+        }
+        document.querySelector(
+          "#character-card > div > ul.character-card-summary > li:nth-child(1) > img"
+        ).src = server;
+      } else {
+        alert("errer");
+      }
+    }
+  };
+  xmlHttp.open(
+    "GET",
+    `https://cors-anywhere.herokuapp.com/http://maple.gg/u/${charName.value}`
+  );
+  xmlHttp.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  xmlHttp.send(null);
+}
+var checkbull = false;
+
+function checkchar() {
+  if (document.querySelector("#character-card") == null) {
+    alert("캐릭터를 먼저 불러와주세요.");
+    return 0;
+  } else {
+    checkbull = true;
+  }
+}
+//값이 변경될때 호출 되는 이벤트 리스너
+function finput(e) {
+  if (checkbull == true) {
+    var file = e.target.files[0]; //선택된 파일
+    var reader = new FileReader();
+    reader.readAsDataURL(file); //파일을 읽는 메서드
+    reader.onload = function () {
+      document.querySelector("#character-card > img.character-background").src =
+        reader.result;
+    };
+  }
+}
+
+// 위 = 38 아래 = 40 오른쪽 = 39 왼쪽 = 37 엔터 = 13
+window.onkeydown = event => {
+  var backname = document.querySelector(
+    "#character-card > img.character-background"
+  );
+  if (event.keyCode == 13) {
+    var charName = document.getElementById("fname");
+    if (charName.value != "" || charName.value != null) {
+      ggcharImg();
+    }
+  } else if (event.keyCode == 37) {
+    if (backname != null) {
+    }
+  }
+  //console.log(event.keyCode);
+};
+
+$(function () {
+  $("#character-card-content").draggable();
+});
+$(function () {
+  $("#character-card").draggable();
+});
+$(function () {
+  $("#character-avatar").draggable();
+});
